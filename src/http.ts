da80ec73ff796501ab35e5bf99f6208775d7bfbd
root <caophi565caophi@gmail.com>
2025-07-29 16:11:53 +0000

    edit

--- src/http.ts
+++ src/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://api-pixcam.hacmieu.xyz",
   headers: {
     "Content-Type": "application/json",
   },
