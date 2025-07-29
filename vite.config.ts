877adc987db1e15a684dd4206e43abd0ee724467
root <caophi565caophi@gmail.com>
2025-07-29 15:56:07 +0000

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3300
-  }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3300,
+    allowedHosts: ['client-pixcam.hacmieu.xyz'],
+  },
 })
